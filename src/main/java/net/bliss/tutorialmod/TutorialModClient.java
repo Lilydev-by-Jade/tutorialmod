73f08cf5790415d4735ed3f484ce06c9b9bf4c32
VoidBLISS <92491008+VoidBLISS@users.noreply.github.com>
2022-09-28 17:50:29 -0500

    added a secret message when you press the "H" key.

--- src/main/java/net/bliss/tutorialmod/TutorialModClient.java
+++ src/main/java/net/bliss/tutorialmod/TutorialModClient.java
@@ -1,6 +1,7 @@
 package net.bliss.tutorialmod;
 
 import net.bliss.tutorialmod.block.ModBlocks;
+import net.bliss.tutorialmod.event.KeyInputHandler;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
@@ -9,5 +10,6 @@ public class TutorialModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.EGGPLAND_CROP, RenderLayer.getCutout());
+        KeyInputHandler.register();
     }
 }
