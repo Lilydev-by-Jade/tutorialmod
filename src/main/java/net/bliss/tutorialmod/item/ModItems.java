d6c7bafae4ee9af94db09ebe92a20a2f61e22b4c
VoidBLISS <92491008+VoidBLISS@users.noreply.github.com>
2022-10-04 18:49:43 -0500

    added the Kaupen Sword

--- src/main/java/net/bliss/tutorialmod/item/ModItems.java
+++ src/main/java/net/bliss/tutorialmod/item/ModItems.java
@@ -5,10 +5,7 @@ import net.bliss.tutorialmod.block.ModBlocks;
 import net.bliss.tutorialmod.item.custom.EightBallItem;
 import net.bliss.tutorialmod.item.custom.EightBallItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.AliasedBlockItem;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryCodecs;
@@ -38,4 +35,7 @@ public class ModItems {
     public static void registerModItems() {
         TutorialMod.LOGGER.debug("Registering Mod Items for " + TutorialMod.MOD_ID);
     }
+    public static final Item KAUPENSWORD = registerItem("kaupensword",
+            new SwordItem(ToolMaterials.DIAMOND, 10, 5f,
+                    new FabricItemSettings().group(ModItemGroup.RUBIDIUM).maxCount(1)));
 }
